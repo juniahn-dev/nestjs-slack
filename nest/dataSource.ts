1fae13fd5db7c3ac5a36332e48ed42da9cc8a241
juniahn-dev <gud0415@gmail.com>
2023-09-03 16:33:37 +0900

    [chore] Add dataSource and db connect

---
+++ nest/dataSource.ts
@@ -0,0 +1,36 @@
+import { ChannelChats } from './src/entities/ChannelChats';
+import { ChannelMembers } from './src/entities/ChannelMembers';
+import { Channels } from './src/entities/Channels';
+import { DMs } from './src/entities/DMs';
+import { DataSource } from 'typeorm';
+import { Mentions } from './src/entities/Mentions';
+import { Users } from './src/entities/Users';
+import { WorkspaceMembers } from './src/entities/WorkspaceMembers';
+import { Workspaces } from './src/entities/Workspaces';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dataSource = new DataSource({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: [
+    ChannelChats,
+    ChannelMembers,
+    Channels,
+    DMs,
+    Mentions,
+    Users,
+    WorkspaceMembers,
+    Workspaces,
+  ],
+  migrations: [__dirname + '/src/migrations/*.ts'],
+  synchronize: false,
+  logging: true,
+});
+
+export default dataSource;
